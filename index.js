8134a1b2fb4d75e50534ec85accb1502ad783aaf
Dayne Dewar <daynedewar@yahoo.com>
2021-02-21 19:35:02 -0500

    commit tree 0f3554a5c10c4f3ff83d71d07aa01c51757b2f39

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solutions in this file
+function writeCards(array, event) {
+    let newArray = []
+    for (let counter = 0; counter < array.length; counter++) {
+        newArray.push(`Thank you, ${array[counter]}, for the wonderful ${event} gift!`);
+    }
+    return newArray;
+}
+
+function countDown(number) {
+    while (number >= 0)
+    console.log(number--);
+}
